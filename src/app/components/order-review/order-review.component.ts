065e8b06bd5bd00c6f15bb13fdbf85689504817a
Bruno Bertomeu <bruno.bertomeu@amadeus.com>
2019-04-29 08:44:22 +0200

    finished order review page

--- src/app/components/order-review/order-review.component.ts
+++ src/app/components/order-review/order-review.component.ts
@@ -1,19 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { ShoppingCartItem } from 'src/app/models/firebase-objects/shopping-cart-item.interface';
+import { OrderService } from 'src/app/services/order.service';
 
 @Component({
   selector: 'app-order-review',
   templateUrl: './order-review.component.html',
-  styleUrls: ['./order-review.component.scss']
+  styleUrls: ['./order-review.component.scss'],
 })
 export class OrderReviewComponent implements OnInit {
   orderId: string;
+  items$: Observable<ShoppingCartItem[]>;
 
-  constructor(private route: ActivatedRoute) {
-    this.orderId = this.route.snapshot.paramMap.get('id');
+  constructor(route: ActivatedRoute, orderService: OrderService) {
+    this.orderId = route.snapshot.paramMap.get('id');
+
+    this.items$ = orderService
+      .get(this.orderId)
+      .pipe(map(order => order.shoppingCartItems));
   }
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
