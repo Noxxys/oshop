f07bf4733785a3916e807d77b15bcdeef049a334
Bruno Bertomeu <bruno.bertomeu@amadeus.com>
2019-04-29 10:58:23 +0200

    my orders WIP

--- src/app/components/my-orders/my-orders.component.ts
+++ src/app/components/my-orders/my-orders.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Order } from 'src/app/models/firebase-objects/order';
+import { AuthService } from 'src/app/services/auth.service';
+import { OrderService } from 'src/app/services/order.service';
 
 @Component({
   selector: 'app-my-orders',
   templateUrl: './my-orders.component.html',
-  styleUrls: ['./my-orders.component.scss']
+  styleUrls: ['./my-orders.component.scss'],
 })
-export class MyOrdersComponent implements OnInit {
+export class MyOrdersComponent {
+  orders$: Observable<Order[]>;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(orderService: OrderService, authService: AuthService) {
+    // TODO: try to log out from each page to see what happens. There should be no error, and potentially some redirects
+    authService.appUser$.subscribe(user => {
+      if (user) {
+        this.orders$ = orderService.getAllForUser(user.id);
+      }
+    });
   }
-
 }
--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@angular/core';
-import { FirebaseCollection } from './firebase-collection';
-import { Order } from '../models/firebase-objects/order';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { map } from 'rxjs/operators';
+import { Order } from '../models/firebase-objects/order';
+import { FirebaseCollection } from './firebase-collection';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class OrderService extends FirebaseCollection<Order> {
   constructor(db: AngularFirestore) {
     super(db, '/orders');
   }
 
-
+  getAllForUser(userId: string): Observable<Order[]> {
+    return super
+      .getAll()
+      .pipe(map(orders => orders.filter(order => order.user.id === userId)));
+  }
 }
