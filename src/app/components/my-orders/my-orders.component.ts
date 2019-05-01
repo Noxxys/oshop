de4d7c668c0d6931287cd3237ed679d9aa58115e
Bruno Bertomeu <bruno.bertomeu@amadeus.com>
2019-05-01 09:26:31 +0200

    my-orders expand WIP

--- src/app/components/my-orders/my-orders.component.ts
+++ src/app/components/my-orders/my-orders.component.ts
@@ -4,19 +4,34 @@ import { Subscription } from 'rxjs';
 import { Order } from 'src/app/models/firebase-objects/order';
 import { AuthService } from 'src/app/services/auth.service';
 import { OrderService } from 'src/app/services/order.service';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-my-orders',
   templateUrl: './my-orders.component.html',
   styleUrls: ['./my-orders.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state(
+        'collapsed',
+        style({ height: '0px', minHeight: '0', display: 'none' })
+      ),
+      state('expanded', style({ height: '*' })),
+      transition(
+        'expanded <=> collapsed',
+        animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')
+      ),
+    ]),
+  ],
 })
 export class MyOrdersComponent implements OnInit, OnDestroy {
-  columnsToDisplay = ['id', 'description', 'date'];
-  fieldsToFilter = ['id', 'description', 'date'];
+  columnsToDisplay = ['products', 'date', 'link'];
+  fieldsToFilter = ['products', 'date', 'link'];
   dataSource: MatTableDataSource<Order>;
   authSubscription: Subscription;
   ordersSubscription: Subscription;
   filterValue: string;
+  expandedOrder: Order | null; // TODO: really necessary? can't any object be null implicitly?
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
@@ -47,5 +62,9 @@ export class MyOrdersComponent implements OnInit, OnDestroy {
     if (this.ordersSubscription) {
       this.ordersSubscription.unsubscribe();
     }
+  }
+
+  OnOrderClicked(order: Order) {
+    this.expandedOrder = this.expandedOrder === order ? null : order;
   }
 }
