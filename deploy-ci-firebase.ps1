2a22a6bb208562c5cd7a921b61d84ca9900cf660
Bruno Bertomeu <bruno.bertomeu@amadeus.com>
2019-06-05 11:04:29 +0200

    use release URL as message when deploying

--- deploy-ci-firebase.ps1
+++ deploy-ci-firebase.ps1
@@ -13,13 +13,17 @@ if ($firebaseProject -eq "") {
     $firebaseProject = $env:FIREBASE_PROJECT;
 }
 
+if ($releaseMessage -eq "") {
+    $releaseMessage = $env:RELEASE_RELEASEWEBURL;
+}
+
 $dir = Split-Path $MyInvocation.MyCommand.Path
 Push-Location $dir
 
 Write-Host "installing Firebase tools..."
 npm i -g firebase-tools
 Write-Host "starting deployment..."
-firebase deploy --token $fireBaseToken --project $fireBaseProject --message "Release: $releaseMessage"
+firebase deploy --token $fireBaseToken --project $fireBaseProject --message $releaseMessage
 Write-Host "deployment completed"
 
 Pop-Location
