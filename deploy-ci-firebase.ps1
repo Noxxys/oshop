98112148091007af5f1782989110b2d9ef7488e1
Bruno Bertomeu <bruno.bertomeu@amadeus.com>
2019-06-05 13:54:42 +0200

    fix env variables

--- deploy-ci-firebase.ps1
+++ deploy-ci-firebase.ps1
@@ -8,9 +8,9 @@ Write-Host token parameter: $firebaseToken
 Write-Host project parameter: $firebaseProject
 Write-Host message parameter: $releaseMessage
 
-Write-Host token env: $firebaseToken
-Write-Host project env: $firebaseProject
-Write-Host message env: $releaseMessage
+Write-Host token env: $env:FIREBASE_AUTH_TOKEN
+Write-Host project env: $env:FIREBASE_PROJECT
+Write-Host message env: $env:RELEASE_RELEASEWEBURL
 
 # if Firebase parameters are not provided, get them from the CI environment variables
 if ($firebaseToken -eq "") {
