ec3efe84008c298b4a07cc808d96ab9e6dd4af06
Bruno Bertomeu <bruno.bertomeu@amadeus.com>
2019-06-11 18:24:17 +0200

    fix build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,49 +1,47 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AngularFireModule } from '@angular/fire';
-import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { CustomFormsModule } from 'ng2-validation';
+import { environment } from 'src/environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { environment } from 'src/environments/environment';
-import { HomeComponent } from './components/home/home.component';
-import { ProductsComponent } from './components/products/products.component';
-import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
-import { CheckOutComponent } from './components/check-out/check-out.component';
-import { OrderSuccessComponent } from './components/order-success/order-success.component';
-import { MyOrdersComponent } from './components/my-orders/my-orders.component';
-import { AdminProductsComponent } from './components/admin/admin-products/admin-products.component';
-import { AdminOrdersComponent } from './components/admin/admin-orders/admin-orders.component';
-import { RouterModule } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
-import { AuthService } from './services/auth.service';
-import { AuthGuardService } from './services/auth-guard.service';
-import { UserService } from './services/user.service';
-import { AdminAuthGuardService } from './services/admin-auth-guard.service';
-import { ProductFormComponent } from './components/admin/product-form/product-form.component';
-import { CategoryService } from './services/category.service';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { ProductService } from './services/product.service';
-import { CustomFormsModule } from 'ng2-validation';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material/material.module';
-import { HeaderComponent } from './components/navigation/header/header.component';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { SidenavListComponent } from './components/navigation/sidenav-list/sidenav-list.component';
-import { ProductCardComponent } from './components/product-card/product-card.component';
-import { ShoppingCartService } from './services/shopping-cart.service';
-import { ShoppingCartItemService } from './services/shopping-cart-item.service';
-import { QuantitySelectorComponent } from './components/quantity-selector/quantity-selector.component';
-import { AddressFormComponent } from './components/address-form/address-form.component';
-import { OrderSummaryComponent } from './components/order-summary/order-summary.component';
-import { OrderReviewComponent } from './components/order-review/order-review.component';
 import { AddressDisplayComponent } from './components/address-display/address-display.component';
+import { AddressFormComponent } from './components/address-form/address-form.component';
+import { AdminOrdersComponent } from './components/admin/admin-orders/admin-orders.component';
+import { AdminProductsComponent } from './components/admin/admin-products/admin-products.component';
+import { ProductFormComponent } from './components/admin/product-form/product-form.component';
+import { CheckOutComponent } from './components/check-out/check-out.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { MyOrdersComponent } from './components/my-orders/my-orders.component';
+import { HeaderComponent } from './components/navigation/header/header.component';
+import { SidenavListComponent } from './components/navigation/sidenav-list/sidenav-list.component';
+import { OrderReviewComponent } from './components/order-review/order-review.component';
+import { OrderSuccessComponent } from './components/order-success/order-success.component';
+import { OrderSummaryComponent } from './components/order-summary/order-summary.component';
+import { ProductCardComponent } from './components/product-card/product-card.component';
+import { QuantitySelectorComponent } from './components/quantity-selector/quantity-selector.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
+import { MaterialModule } from './material/material.module';
+import { AdminAuthGuardService } from './services/admin-auth-guard.service';
+import { AuthGuardService } from './services/auth-guard.service';
+import { AuthService } from './services/auth.service';
+import { CategoryService } from './services/category.service';
+import { ProductService } from './services/product.service';
+import { ShoppingCartItemService } from './services/shopping-cart-item.service';
+import { ShoppingCartService } from './services/shopping-cart.service';
+import { UserService } from './services/user.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ProductsComponent,
     ShoppingCartComponent,
     CheckOutComponent,
     OrderSuccessComponent,
@@ -72,10 +70,8 @@ import { AddressDisplayComponent } from './components/address-display/address-di
     CustomFormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent },
-      { path: 'products', component: ProductsComponent },
       { path: 'shopping-cart', component: ShoppingCartComponent },
       { path: 'login', component: LoginComponent },
-
       {
         path: 'check-out',
         component: CheckOutComponent,
@@ -96,7 +92,6 @@ import { AddressDisplayComponent } from './components/address-display/address-di
         component: OrderReviewComponent,
         canActivate: [AuthGuardService],
       },
-
       {
         path: 'admin/products/new',
         component: ProductFormComponent,
@@ -112,7 +107,6 @@ import { AddressDisplayComponent } from './components/address-display/address-di
         component: AdminProductsComponent,
         canActivate: [AuthGuardService, AdminAuthGuardService],
       },
-
       {
         path: 'admin/orders',
         component: AdminOrdersComponent,
